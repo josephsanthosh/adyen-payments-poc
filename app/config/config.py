7c44efc8a2fee6118d403dd14ec22b472e1ad9e4
Santhosh Joseph <josephsanthosh.code@gmail.com>
2020-05-22 15:07:33 +0200

    Created config manager

---
+++ app/config/config.py
@@ -0,0 +1,15 @@
+
+merchant_account = ''
+api_key = ''
+origin_key = '' 
+server_name = ''
+
+def get_all_config():
+    global merchant_account,api_key,origin_key,server_name
+    server_name = 'http://josephsanthosh.pythonanywhere.com'
+    #server_name = 'http://localhost:8080'
+    merchant_account = 'SupportRecruitementCOM'
+    api_key = 'AQE1hmfxKo3NaxZDw0m/n3Q5qf3Ve55dHZxYTFdTxWq+l3JOk8J4BO7yyZBJ4o0JviXqoc8j9sYQwV1bDb7kfNy1WIxIIkxgBw==-q7XjkkN/Cud0WELZF+AzXpp/PuCB8+XmcdgqHYUWzTA=-Kk9N4dG837tIyjZF'
+    origin_key = 'pub.v2.7814286629520534.aHR0cDovL2pvc2VwaHNhbnRob3NoLnB5dGhvbmFueXdoZXJlLmNvbQ.vKbntfieKPxFs_z4GFJL3otJD-pHp24P3tQPMUS7wqw'
+    #origin_key = 'pub.v2.7814286629520534.aHR0cDovL2xvY2FsaG9zdDo4MDgw.gKjRMbdysLhnVHPmtYDJ0ufSr7CL-ttXhunsdwc0qjE'
+    
